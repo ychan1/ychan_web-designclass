6dad43cd5feadff2cb89562f6d1d1d41840d4a46
ychan1 <77257030+ychan1@users.noreply.github.com>
2021-01-30 23:59:22 +0800

    project2start

---
+++ project-02/index.js
@@ -0,0 +1,22 @@
+$("document").ready(function() {
+
+$("#bread1").click(function(){
+        $("#bread1").hide();
+        $("#bread2").show().css("animation-play-state", "running");
+        })
+// $("#bread1").click(function(){
+//         $("#bread1").toggle(
+//             function(){
+//             $("#bread1").hide();
+//             $("#bread2").show().css("animation-play-state", "running");},
+//             function(){
+//             $("#bread2").hide();
+//             $("#bread1").show();}
+            
+//         );
+       
+// });   
+                
+    
+    
+});
